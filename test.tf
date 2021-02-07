f6fdce1d92e9f2d316860694df3940db2061960b
vbkr1598 <68968706+vbkr1598@users.noreply.github.com>
2021-02-07 16:42:27 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -15,8 +15,55 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_key_pair" "ubuntu" {
+  key_name   = "ubuntu"
+  public_key = file(var.key_p)
+}
+output "AWS_Link" {
+  //value = concat([aws_instance.ubuntu.public_dns,""],[":8080/spring-mvc-example",""])
+  value=format("Access the AWS hosted app from here: %s%s", aws_instance.ubuntu.public_dns, ":8080/spring-mvc-example")
+}
+resource "aws_instance" "ubuntu" {
+  key_name      = aws_key_pair.ubuntu.key_name
+  ami           = "ami-0a0ad6b70e61be944"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ubuntu"
+  }
+
+  /*vpc_security_group_ids = [
+    aws_security_group.ubuntu.id
+  ]*/
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file(var.key_p2)
+    host        = self.public_ip
+  }
+  
+  
+  user_data = <<-EOF
+  #!bin/bash
+  sudo amazon-linux-extras install tomcat8.5 
+  sudo systemctl enable tomcat
+  sudo systemctl start tomcat
+  cd /usr/share/tomcat/webapps/
+  sudo cp /tmp/spring-mvc-example.war /usr/share/tomcat/webapps/spring-mvc-example.war
+  EOF
+  
+  provisioner "file" {
+    source      = "/var/lib/jenkins/workspace/vib_assign/target/spring-mvc-example.war"
+    destination = "/tmp/spring-mvc-example.war"
+    connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file(var.key_p2)
+    host        = self.public_ip
+  }
+  }
+  
 }
 
 variable "acc" {
@@ -24,5 +71,14 @@ variable "acc" {
 }
 
 variable "sec" {
+  type = string
+}
+
+variable "key_p" {
+  type = string
+}
+
+
+variable "key_p2" {
   type = string
 }
